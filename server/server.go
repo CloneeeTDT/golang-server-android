508e7f3750720670712e0d43f2acd7169adb1ae8
HuyNguyen <51800781@student.tdtu.edu.vn>
2023-04-07 02:17:31 +0700

    feat: fix server config

--- server/server.go
+++ server/server.go
@@ -1,8 +1,11 @@
 package server
 
+import "golang-server-android/config"
+
 func Init() {
+	c := config.GetConfig()
 	r := NewRouter()
-	err := r.Run() // Run in 8080
+	err := r.Run(c.GetString("server.port")) // Run in 8080
 	if err != nil {
 		return
 	}
